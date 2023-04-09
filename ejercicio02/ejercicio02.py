bb01dfe1cfe55891a3c19640fea88b01ad6af168
BobiStudio <mgonzgarc@myuax.com>
2023-04-10 00:32:38 +0200

    mini cambio

--- ejercicio02/ejercicio02.py
+++ ejercicio02/ejercicio02.py
@@ -17,8 +17,13 @@ print("?")
 
 
 '''
-El objeto yang se atribuye a la clase yin, en la línea 8. Al ejecutar __del__(), se ejecuta del(yang), que elimina el objeto yang. Sin embargo, el objeto yang sigue existiendo en la clase yin, por lo que no se elimina instantáneamente. Entonces cuando se ejecuta print("?"), todavia se está ejecutando el método del.
+El objeto yang se atribuye a la clase yin, en la línea 8. Al ejecutar __del__(), se ejecuta del(yang), 
+que elimina el objeto yang. Sin embargo, el objeto yang sigue existiendo en la clase yin, 
+por lo que no se elimina instantáneamente. Entonces cuando se ejecuta print("?"), 
+todavia se está ejecutando el método del.
 
-Para conseguir que se muestre antes el mensaje de Yang destruido, habria que establecer antes del print("?") la siguiente línea de código: yin.yang = None; debido a que al establecer el atributo de la clase yin a None, se elimina el objeto yang de la clase yin, por lo que se ejecuta el método del.
+Para conseguir que se muestre antes el mensaje de Yang destruido, habria que establecer antes del print("?") 
+la siguiente línea de código: yin.yang = None; debido a que al establecer el atributo de la clase yin a None,
+se elimina el objeto yang de la clase yin, por lo que se ejecuta el método del.
 
 '''
